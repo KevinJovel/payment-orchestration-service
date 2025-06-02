f5aa93f4cec9b43314a90a735ca6d0ebe64f70ef
Kevin Jovel <kevinjovel9@gmail.com>
2025-06-02 14:01:06 -0600

    feat: add Customer and Payment entity classes with JPA mappings

---
+++ src/main/java/com/payment/orchestration/service/payment_orchestration_service/entity/Customer.java
@@ -0,0 +1,68 @@
+package com.payment.orchestration.service.payment_orchestration_service.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @Size(min = 1, max = 36)
+    @Column(name = "customer_id", nullable = false, length = 20)
+    private String customerId; 
+    @NotBlank
+    @Size(max = 150)
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(name = "email", nullable = false, unique = true, length = 150)
+    private String email;
+
+    // KJ: Un cliente puede tener muchos pagos
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Payment> payments;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Set<Payment> payments) {
+        this.payments = payments;
+    }
+}
