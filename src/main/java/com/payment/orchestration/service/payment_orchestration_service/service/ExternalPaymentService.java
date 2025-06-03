7a5b6894355a4f1beb6c8f80ce07fcc44e7ccbd4
Kevin Jovel <kevinjovel9@gmail.com>
2025-06-03 16:30:19 -0600

    feat: skip ExternalApyment service, not in use

--- src/main/java/com/payment/orchestration/service/payment_orchestration_service/service/ExternalPaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.payment.orchestration.service.payment_orchestration_service.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ExternalPaymentService {
-
-}
